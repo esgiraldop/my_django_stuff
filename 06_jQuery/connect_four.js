e582b93b7539283155cdff14534ae80d49162a31
Erick Giraldo <erickgiraldo94@gmail.com>
2023-11-04 02:50:09 -0500

    Finished coloring chips by player

--- 06_jQuery/connect_four.js
+++ 06_jQuery/connect_four.js
@@ -1,19 +1,29 @@
-var player1 = prompt("Player One: Enter Your Name, you will be Blue")
-var player2 = prompt("Player Two: Enter Your Name, you will be Red")
-var color1 = "blue"
-var color2 = "red"
+let player1 = prompt("Player One: Enter Your Name, you will be Blue")
+let player2 = prompt("Player Two: Enter Your Name, you will be Red")
+let color1 = "blue"
+let color2 = "red"
+let cells = $("td")
 
-var orgnlPlayerInfo = $("h4")
-var playerInfo = orgnlPlayerInfo
-var lastRowArray = [0, 0, 0, 0, 0, 0, 0] // Every position is the column number.
-                                                // Every number is the next row to be filled in the table
+let chipAlready = Array(cells.length).fill(0) // To track what cells are filled with a chip already
+let orgnlPlayerInfo = $("h4")
+let orgnlBckgrndColor = $("td").eq(0).css("background-color")
+let playerInfo = orgnlPlayerInfo
+let cell_index
+
+// To protect from user entering an empty name
+if (player1 === ""){
+    player1 = "  "
+}
+if (player2 === ""){
+    player2 = "  "
+}
 
 function change_playerInfo(){
     if (playerInfo.text().includes("(Player name)")) {
         playerInfo.text(playerInfo.text().replace("(Player name)", player1)); //Changing name
         playerInfo.text(playerInfo.text().replace("(Chip color)", color1));
     }
-        else if(playerInfo.text().includes(player2)){
+    else if(playerInfo.text().includes(player2)){
         playerInfo.text(playerInfo.text().replace(player2, player1)); //Changing name
         playerInfo.text(playerInfo.text().replace(color2, color1));
     }else if(playerInfo.text().includes(player1)){
@@ -21,34 +31,45 @@ function change_playerInfo(){
         playerInfo.text(playerInfo.text().replace(color1, color2));
     }
 }
-
+function scan_chipAlready (background_color){
+    var start = 6*6-1+cell_index // always start from the bottom
+    for(var i=start; i>=0; i-=7){
+        if(cells.eq(i).css("background-color") === orgnlBckgrndColor){
+            cells.eq(i).css({"background-color":background_color})
+            break
+        }
+    }
+}
 function change_color(event){
-    var lastRow = lastRowArray[j]
-    if (playerInfo.text().includes("(Player name)")) {
-        event.target.eq(lastRow).children().eq(j).css({"color":"blue"})
-    }
-        else if(playerInfo.text().includes(player2)){
-        event.target.eq(lastRow).children().eq(j).css({"color":"blue"})
+    var background_color = ""
+    if (playerInfo.text().includes("(Player name)") | playerInfo.text().includes(player2)) {
+        background_color = "rgb(86, 151, 255)";
     }else if(playerInfo.text().includes(player1)){
-        event.target.eq(lastRow).children().eq(j).css({"color":"red"})
+        background_color = "rgba(237, 45, 73, 0.8)"
     }
-
+    // Here, I would call "scan_chipAlready()" to scan through the cells that have a chip already.
+        // I would scan backwards, starting from the cell I clicked, and scanning every 7 positions (The next cell above)
+    console.log("When clicking the cell, the column number clicked is: ", cell_index)
+    td_num = scan_chipAlready (background_color) // Still something to fix here
+    console.log("When clicking the cell, the td number is: ", td_num)
 }
+
 // First turn
-change_playerInfo(player1, color1)
+change_playerInfo()
+$('table').on('click', function(event) {
+    // Capture the click event on the table itself
+    console.log('Table clicked');
+    cell_index = event.target.cellIndex
+    console.log("When clicking table, the column number clicked is: ", cell_index)
 
-$("td").click(change_playerInfo)
-
-var rows = $("tr")
-
-for(var i=0; i<rows.length; i++){
-    var cols = rows.eq(i).children()
-    for(var j=0;j<cols.length; j++){
-        rows.click(change_color) // Accessing to rows and then to columns
-        lastRowArray[j] = lastRowArray[j] + 1
-        // somehow, use "lastRowArray" to know what is the next row to fill in the current column and store the new
-        // number.
+    // Check if the click event target is a table cell
+    if ($(event.target).is('td')) {
+        change_playerInfo()
+        change_color(event);
     }
-}
 
-console.log(lastRowArray)
+    // Checking horizontally if there are four consecutive chips of the same color
+
+    // Checking vertically if there are four consecutive chips of the same color
+
+});
