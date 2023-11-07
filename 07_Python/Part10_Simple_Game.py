bfe89965b343fe47df68efe1ccde71b147713ccd
Erick Giraldo <erickgiraldo94@gmail.com>
2023-11-06 21:16:55 -0500

    Finished guess number project

--- 07_Python/Part10_Simple_Game.py
+++ 07_Python/Part10_Simple_Game.py
@@ -18,10 +18,6 @@
 # 4. Based on these clues you will guess again until you break the code with a
 #    perfect match!
 
-# There are a few things you will have to discover for yourself for this game!
-# Here are some useful hints:
-
-# Try to figure out what this code is doing and how it might be useful to you
 import random
 
 def gen_num():
@@ -34,10 +30,77 @@ def get_input():
     while incorrect_length:
         guess = input("What is your guess? ")
         if len(guess) != 3:
-            print("Only a three-digit number is allowed")
+            print("Only a 3-digit number is allowed\n")
         else:
-            incorrect_length = False
-
-    guess = [int(x) for x in ' '.join(guess).split(" ")]
+            repeated_num = True
+            while repeated_num:
+                guess = [int(x) for x in ' '.join(guess).split(" ")]
+                if len(set(guess)) < 3:
+                    print("Please enter a number with no repeating digits\n")
+                    break
+                else:
+                    incorrect_length = False
+                    repeated_num = False
     return guess
 
+def check_nums(pc_guess, usr_guess):
+    match_sum = 0
+    close_sum = 0
+    match_idxs = []
+    message = "You obtained "
+    # Checking for matches
+    for i in range(len(pc_guess)):
+        if pc_guess[i] == usr_guess[i]:
+            match_sum += 1
+            match_idxs.append(i)
+    #Checking for closes
+    for i in range(len(pc_guess)):
+        if i in match_idxs:
+            continue
+        for j in range(len(pc_guess)):
+            if j in match_idxs:
+                continue
+            if usr_guess[i] == pc_guess[j]:
+                close_sum += 1
+
+    if match_sum > 0 or close_sum > 0:
+        if match_sum > 0 and close_sum > 0:
+            if match_sum == 1:
+                message = message + "1 match"
+                if close_sum == 1:
+                    message = message + " and 1 close"
+                elif close_sum == 2:
+                    message = message + " and 2 close"
+            elif match_sum == 2:
+                message = message + "2 matches"
+                # If there are three matches, this function should not even be called
+        elif match_sum > 0 and close_sum == 0:
+            if match_sum == 1:
+                message = message + "1 match"
+            elif match_sum == 2:
+                message = message + "2 matches"
+        elif match_sum == 0 and close_sum > 0:
+            if close_sum == 1:
+                message = message + "1 close"
+            elif close_sum == 2:
+                message = message + "2 close"
+
+        message = message + "!\n"
+        print(message)
+    else:
+        message = message + "no close digits"
+        print(message)
+
+
+game_on = True
+pc_guess = gen_num()
+print("The pc guess is: ", pc_guess)
+while game_on:
+    usr_guess = get_input()
+    #print(usr_guess)
+
+    if pc_guess == usr_guess:
+        print("Perfect match!")
+        break
+    else:
+        check_nums(pc_guess, usr_guess)
